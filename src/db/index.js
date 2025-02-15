e088fd48a4ed990c644a09ce9b8c453d96fa8edd
uditkumar08 <udit2162@gmail.com>
2025-02-16 00:25:44 +0530

    Your commit message

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDb = async () => {
+    try{
+        const MONGO_URI = `${process.env.MONGODB_URI}/${DB_NAME}`;
+        const connectionInstance = await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+     console.log(`\n MongoDB connected ! DB host : 
+        ${connectionInstance.connection.host}`);
+     
+
+    }catch(error){
+        console.log("MongoDb Connection error ", error);
+        process.exit(1);
+    }
+}
+export default connectDb
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import dotenv from "dotenv"
 import {app} from "./app.js"
+import connectDb from "./db/index.js"
 dotenv.config({
-    path: "./'env"
+    path: "./.env"
 })
 const PORT = process.env.PORT || 7000
-app.listen(PORT, () =>{
-    console.log(`Sever is running on port ${PORT}`);
+
+
+connectDb()
+.then(()=>{
+    app.listen(PORT, () =>{
+            console.log(`Sever is running on port ${PORT}`);
+            
+})
+})
+.catch((err)=>{
+    console.log(`MongoDB connection error ` ,err);
     
 })
+
+
+// app.listen(PORT, () =>{
+//     console.log(`Sever is running on port ${PORT}`);
+    
+// })
